05960efec64bbd282e39524c9f96a75f7834183a
Aaron Williams <aaron.colby.williams@gmail.com>
2014-10-26 10:09:40 -0700

    it basically works

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,4 @@
-/* global Wit: false, toString: false, jQuery: false, alert: false */
+/* global Wit: false, toString: false, jQuery: false, alert: false, Handlebars: false */
 
 'use strict';
 
@@ -15,10 +15,12 @@ Concierge = {
     mic: new Wit.Microphone(document.getElementById('microphone')),
     init: function () {
         var self = this;
+        //self.download();
         self.setup();
         self.mic.connect('CSDL5LYIMJ77EM66K6TDF5HQ5AWM3ZYL');
     },
     fetch: function (aQuery) {
+        var self = this;
         $.ajax({
             crossDomain: true,
             url: 'http://newsautomata.starbase.in/search',
@@ -26,6 +28,7 @@ Concierge = {
             data: {query: aQuery},
         })
         .done(function(response) {
+            self.configure('#tmpl', '#placeholder', response);
             console.log('success');
         })
         .fail(function() {
@@ -101,6 +104,43 @@ Concierge = {
     },
     error: function (msg) {
         document.getElementById('error').innerHTML = msg;
+    },
+    download: function () {
+        var manifest_url = location.href + 'manifest.webapp';
+
+        function install(ev) {
+          ev.preventDefault();
+          // define the manifest URL
+          // install the app
+          var installLocFind = navigator.mozApps.install(manifest_url);
+          installLocFind.onsuccess = function(data) {
+            // App is installed, do something
+          };
+          installLocFind.onerror = function() {
+            // App wasn't installed, info is in
+            // installapp.error.name
+            alert(installLocFind.error.name);
+          };
+        };
+
+        // get a reference to the button and call install() on click if the app isn't already installed. If it is, hide the button.
+        var button = document.getElementById('install-btn');
+
+        var installCheck = navigator.mozApps.checkInstalled(manifest_url);
+
+        installCheck.onsuccess = function() {
+          if(installCheck.result) {
+            button.style.display = "none";
+          } else {
+            button.addEventListener('click', install, false);
+          };
+        };
+    },
+    configure: function (template, placeholder, obj) {
+        var source = $(template).html(),
+            hbs = Handlebars.compile( source );
+
+        $(placeholder).html( hbs( obj ) );
     }
 
 };
